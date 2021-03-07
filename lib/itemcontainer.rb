7bc107204668086990e8ea453eb41b1546dc8aad
tta2yta <tta2yta@gmail.com>
2021-03-08 00:30:03 +0300

    Craete itemcontainer class with data arrays for jokes and inspiration qoutes

--- lib/itemcontainer.rb
+++ lib/itemcontainer.rb
@@ -1,8 +1,12 @@
 class ItemContainer
+  attr_reader :insp_quote
+  attr_reader :jokes
+
   def initialize
     @insp_quote = ['All our dreams can come true, if we have the courage to pursue them.',
                    'The secret of getting ahead is getting started.',
                    'It’s hard to beat a person who never gives up.']
+
     @jokes = ['Where do you find a cow with no legs? Right where you left it',
               "Why aren't koalas actual bears? They don't meet the koalafications.",
               "What's E.T. short for? Because he's only got little legs."]
