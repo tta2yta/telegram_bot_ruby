7d356d25fa618c55eac265a0c8827e2baa00f778
tta2yta <tta2yta@gmail.com>
2021-03-08 23:48:27 +0300

    Modify telegram_bot_spec file

--- spec/telegram_bot_spec.rb
+++ spec/telegram_bot_spec.rb
@@ -11,21 +11,27 @@ describe ItemContainer do
     end
   end
   describe '#gen_qoutes' do
-    it 'Generatting random Quotes' do
+    it 'Generatting random Quotes and should return Array' do
       expect(itm_con.gen_qoutes.class).to eql(Array)
+    end
+    it 'Generatting random Quotes and should return Array' do
+        expect(itm_con.gen_qoutes.class).not_to eql(Hash)
     end
   end
 
   describe '#gen_jokes' do
-    it 'Generatting random jokes' do
+    it 'Generatting random jokes and should return Hash' do
       usr.user_name('Ted')
       expect(itm_con.gen_jokes.class).to eql(Hash)
     end
   end
 
   describe '#eru_exchange' do
-    it 'Generatting Exchange Rates for EUR to other Currencies' do
+    it 'Generatting Exchange Rates for EUR to other Currencies and should return hash' do
       expect(itm_con.eru_exchange.class).to eql(Hash)
     end
+    it 'Generatting Exchange Rates for EUR to other Currencies and should return hash' do
+        expect(itm_con.eru_exchange.class).not_to eql(Array)
+      end
   end
 end
