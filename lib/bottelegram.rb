01331acbb5b1545ebb4b37fdf9599aff3febb2be
tta2yta <tta2yta@gmail.com>
2021-03-08 09:44:33 +0300

    Add switch to bottelegram class

--- lib/bottelegram.rb
+++ lib/bottelegram.rb
@@ -6,14 +6,23 @@ class BotTelegram
 
   def bot_main
     item_con = ItemContainer.new
+    usr = User.new
     index = rand(0..3)
     index -= 1 if index == 3
     p item_con.insp_quote[index]
+    bot = Telegram::Bot::Client.new(TOKEN)
+    bot.api.send_message(chat_id: "kkk23434", text: item_con.insp_quote[index])
     Telegram::Bot::Client.run(TOKEN) do |bot|
+      # bot.api.send_message(chat_id: usr.id, text: item_con.insp_quote[index])
       bot.listen do |message|
         index = rand(0..3)
         index -= 1 if index == 3
-        bot.api.send_message(chat_id: message.chat.id, text: item_con.jokes[index])
+        case message.text
+        when '/start'
+          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+        when '/stop'
+          bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+        end
       end
     end
   end
--- lib/user.rb
+++ lib/user.rb
@@ -1,8 +1,10 @@
 class User
   attr_reader :name
+  attr_reader :id
 
   def initialize
     @name = ''
+    @id=432
   end
 
   def user_name(name)
