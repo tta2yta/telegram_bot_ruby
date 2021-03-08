c0a795bc992201a4f33710ee11c2a0428641044d
tta2yta <tta2yta@gmail.com>
2021-03-08 18:43:16 +0300

    Create rspec file and defined test for user_name

--- spec/telegram_bot_spec.rb
+++ spec/telegram_bot_spec.rb
@@ -4,8 +4,8 @@ require './lib/user'
 
 describe ItemContainer do
   let(:itm_con) { ItemContainer.new }
-  let(:usr) { User.new }
-  describe '#gen_qoutes' do
+  let(:usr){User.new}
+  describe '#user_name' do
     it 'Setting User Name' do
       expect(usr.user_name('Ted')).to eql('Ted')
     end
